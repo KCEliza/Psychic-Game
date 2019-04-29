6380ba4d3e74e19c91828ed0b66fce435de54c95
KCEliza <48337639+KCEliza@users.noreply.github.com>
2019-04-29 09:20:31 -0600

    Initial variables complete

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+var compChoice = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var win = 0;
+var lose = 0;
+
+var userText = document.getElementById("userGuess");
+var compText = document.getElementById("compGuess");
+var winHtml = document.getElementById("win");
+var lossHtml = document.getElementById("loss");
