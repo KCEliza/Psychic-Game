8c79dae218b7756cee055cc490c79ecdacd99cc8
KCEliza <48337639+KCEliza@users.noreply.github.com>
2019-04-29 21:23:17 -0600

    Counts wins, prints user input

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,49 +3,57 @@ var compChoice = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m
 // keeping score
 var win = 0;
 var lose = 0;
-var guesses = 9;
 //id's from html
 var userText = document.getElementById("userGuess");
 var winHtml = document.getElementById("win");
 var lossHtml = document.getElementById("loss");
 var directions = document.getElementById("direction");
 var guessesLeft = document.getElementById("guessLeft");
-//randomize the computer choice
 
 
+//ASK HOW TO FIND VIDEO ABOUT FUNCTIONS> You need a function here but it breaks all your stuff.
+    for (let guesses = 9; guesses > -1; guesses--) {
+        
+        document.onkeyup = function (event) {
+            //randomize the computer choice
+            var compKey = compChoice[Math.floor(Math.random() * compChoice.length)];
 
-document.onkeyup = function (event) {
-    var compKey = compChoice[Math.floor(Math.random() * compChoice.length)];
+            var userKey = event.key;
+            if (userKey === "a" || userKey === "b" || userKey === "c" ||
+                userKey === "d" || userKey === "e" || userKey === "f" ||
+                userKey === "g" || userKey === "h" || userKey === "i" ||
+                userKey === "j" || userKey === "k" || userKey === "l" ||
+                userKey === "m" || userKey === "n" || userKey === "o" ||
+                userKey === "p" || userKey === "q" || userKey === "r" ||
+                userKey === "s" || userKey === "t" || userKey === "u" ||
+                userKey === "v" || userKey === "w" || userKey === "x" ||
+                userKey === "y" || userKey === "z") {
+                console.log(userKey);
+                console.log(guesses);
+                userText.textContent = (userKey);
 
-    var userKey = event.key;
-    if (userKey === "a" || userKey === "b" || userKey === "c" ||
-        userKey === "d" || userKey === "e" || userKey === "f" ||
-        userKey === "g" || userKey === "h" || userKey === "i" ||
-        userKey === "j" || userKey === "k" || userKey === "l" ||
-        userKey === "m" || userKey === "n" || userKey === "o" ||
-        userKey === "p" || userKey === "q" || userKey === "r" ||
-        userKey === "s" || userKey === "t" || userKey === "u" ||
-        userKey === "v" || userKey === "w" || userKey === "x" ||
-        userKey === "y" || userKey === "z") {
-        console.log(userKey);
-        console.log(guesses);
-        if(userKey === compKey){
-            win++
-            console.log(win);
-        }
-        else if(userKey !== compKey){
-            guesses--
-            console.log(guesses);
-            //NEED TO ADD A FOR LOOP TO STEP IN HERE SOME HOW
-            if(guesses === 0){
-                guesses = 9
-                lose++
+                if (userKey === compKey) {
+                    win++
+                    winHtml.textContent = (win);
+                    
+
+                }
+                else if (userKey !== compKey) {
+                    guessesLeft.textContent = (guesses);
+                    //NEED TO ADD A FOR LOOP TO STEP IN HERE SOME HOW
+                
+
+
+                // if (guesses > -1) {
+                //     lose++
+                //     lossHtml.textContent = (lose);
+                // }
             }
+
+            }
+            else {
+                alert("Choose a letter to play!");
+            }
+
         }
     }
-    else {
-    alert("Choose a letter to play!");
-}
-
-
-}
